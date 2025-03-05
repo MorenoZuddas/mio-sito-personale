b2470a727c65e64438fb7bcdcb669bbf73bdd240
Zuddas, Moreno <mzuddas@deloitte.it>
2025-03-05 15:52:06 +0100

    first commit

---
+++ react-ts/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Pagina principale</h2>
+            <p>Benvenuto nel mio sito personale!</p>
+        </div>
+    );
+};
+
+export default Home;
